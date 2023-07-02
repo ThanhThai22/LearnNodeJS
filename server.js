ad3cbd71407c3787f6fd080c5175244e9f792d2c
zero <accnayxaimai22@gmail.com>
2023-07-02 12:33:33 +0700

    express app

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello World!</h3>');
-    res.end();
+// const server = http.createServer((req, res) => {
+//     console.log('run request ...')
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h3>Hello World!</h3>');
+//     res.end();
+// })
+
+// server.listen(3000, 'localhost', () => {
+//     console.log('NodeJS server is running on port: 3000');
+// })
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! thai thanh')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('NodeJS server is running on port: 3000');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
